bbf7d40953009b63706a116fbd5345eb35aa147e
sinajr <jamyarisina@gmail.com>
2024-04-16 12:27:36 +0300

    buge is fixed

--- MiniProject3/javascript.js
+++ MiniProject3/javascript.js
@@ -1,10 +1,10 @@
 let $=document;
-let userIpt=Number(prompt("inter your age"));
+let userIpt=Number(prompt("Enter your age"));
 let lichanger=$.querySelectorAll('.lis');
 if(userIpt>18){
     lichanger[0].innerHTML="your age is enough for bars";
-}if (userIpt===18) {
+}else if(userIpt===18) {
     lichanger[1].innerHTML="your age is equal to 18 you can go";
 } else {
-    lichanger[2].innerHTML="your age is undder 18 you are so young"
+    lichanger[2].innerHTML="your age is undder 18 you are so young";
 }
