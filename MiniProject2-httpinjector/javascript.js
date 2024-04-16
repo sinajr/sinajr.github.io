197348e0c0a6423d04eee118add481dd112bd88e
sinajr <jamyarisina@gmail.com>
2024-04-16 10:53:36 +0300

    bug is fixed

--- MiniProject2-httpinjector/javascript.js
+++ MiniProject2-httpinjector/javascript.js
@@ -1,11 +1,13 @@
 let $=document;
 let btnpromt=$.getElementById("btn-promt");
+let uLink=$.querySelector(".u-link");
 btnpromt.addEventListener("click",()=>{
     let userlink=prompt("Enter your URL");
     let userURL=userlink.toLowerCase();
     if(userURL.slice(0,4)=="http"){
         alert('link is true')
     }else{
-        alert("your link is not contain http but dont worry ;)\n \n \n"+"https//"+userURL+"\n or"+"\n http//"+userURL)
+        alert("your link is not contain http but dont worry ;)\n \n \n"+"https//"+userURL+"\n or"+"\n http//"+userURL);
+     uLink.innerHTML="<a"+" "+"href='"+"https://"+userURL+"'"+">"+"https://"+userURL+"</a>";
     }
 })
