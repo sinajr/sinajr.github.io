8910f0f6ccc7bccf6d07ae5ce7e89e8aeccc68c6
sinajr <jamyarisina@gmail.com>
2024-04-18 13:43:43 +0300

    calculate practice

--- MiniProject4/javascript.js
+++ MiniProject4/javascript.js
@@ -1,10 +1,74 @@
 let $=document;
 let btns=$.querySelectorAll('.btn-cal');
-let showCal=$.getElementById('cal-res')
+let showCal=$.getElementById('cal-res');
+let result;
+showCal.addEventListener('keydown',(e)=>{
+    if(e.key==="Enter"){
+        showCal.value=result
+    }
+})
 btns.forEach(function(cal){
-    let btnVal=cal.addEventListener('click',()=>{
-        let valbtn=cal.value;
-        let holderVal=showCal.value=valbtn;
-        
+    cal.addEventListener('click',(e)=>{
+        cal.classList.add("bg-newback");
+        let btnfocus=e.target.dataset.value;
+        showCal.value+=btnfocus;
+        if(btnfocus="+"){
+            let a=Number(showCal.value);
+            showCal.value="";
+            console.log(a);
+        }
+        // switch(calCulat){
+        //     case calCulat="1":
+        //         showCal.value="1"
+        //     break;
+        //     case calCulat="2":
+        //         showCal.value="2"
+        //     break;
+        //     case calCulat="3":
+        //         showCal.value="3"
+        //     break;
+        //     case calCulat="4":
+        //         showCal.value="4"
+        //     break;
+        //     case calCulat="5":
+        //         showCal.value="5"
+        //     break;
+        //     case calCulat="6":
+        //         showCal.value="6"
+        //     break;
+        //     case calCulat="7":
+        //         showCal.value="7"
+        //     break;
+        //     case calCulat="8":
+        //         showCal.value="8"
+        //     break;
+        //     case calCulat="9":
+        //         showCal.value="9"
+        //     break;
+        //     case calCulat="0":
+        //         showCal.value="0"
+        //     break;
+        //     case calCulat="=":
+        //         showCal.value="="
+        //     break;
+        //     case calCulat="*":
+        //         showCal.value="*"
+        //     break;
+        //     case calCulat="-":
+        //         showCal.value="-"
+        //     break;
+        //     case calCulat="+":
+        //         showCal.value="+"
+        //     break;
+        //     case calCulat="/":
+        //         showCal.value="/"
+        //     break;
+        //     case calCulat="%":
+        //         showCal.value="%"
+        //     break;
+        //     case calCulat="C":
+        //         showCal.value=""
+        //     break;
+        // }
     })
 })
