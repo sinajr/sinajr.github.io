380738ed40e20236f2fc73f12547d20a6b64ed70
sinajr <jamyarisina@gmail.com>
2024-04-26 13:25:04 +0300

    new js program

---
+++ bootstrap/javascript/java.js
@@ -0,0 +1,12 @@
+let star= "#";
+let btnStart=document.getElementById('btn-start')
+btnStart.addEventListener('click',()=>{
+    for(let i=0;i<=30;i++){
+        if((i%2)==0){
+            console.log(star)
+        }else{
+            console.log(star+' '+star)
+        }
+    }
+})
+
