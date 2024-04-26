ded465271209c91c566a3371afb2e271d47c12e2
sinajr <jamyarisina@gmail.com>
2024-04-26 13:43:04 +0300

    ###

--- bootstrap/javascript/java.js
+++ bootstrap/javascript/java.js
@@ -3,9 +3,9 @@ let btnStart=document.getElementById('btn-start')
 btnStart.addEventListener('click',()=>{
     for(let i=0;i<=30;i++){
         if((i%2)==0){
-            console.log(star)
+            (console.log(star.repeat(i/2)))
         }else{
-            console.log(star+' '+star)
+            (console.log((star+' ').repeat(i)))
         }
     }
 })
