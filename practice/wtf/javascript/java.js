3601344c41aea1354b2d86b0f465000c30cac0bd
sinajr <jamyarisina@gmail.com>
2024-05-15 18:27:47 +0300

    timer

--- practice/wtf/javascript/java.js
+++ practice/wtf/javascript/java.js
@@ -7,3 +7,9 @@ function pickTheColor(){
     let choosenColor= colorPicker.value;
     document.documentElement.style.setProperty('--box-background',choosenColor);
 }
+let timer=$.getElementById('timer');
+setInterval(function(){
+    let myTime=new Date();
+    let liveTime=""+myTime.getHours()+" : "+myTime.getMinutes()+" : "+myTime.getSeconds(s)
+    timer.innerHTML=liveTime;
+},1000)
