b0ff9f16ff177eb8aea02e1e2c0e90d5d9a99131
sinajr <jamyarisina@gmail.com>
2024-05-28 11:04:10 +0300

    update

--- practice/bootstrap/javascript/java.js
+++ practice/bootstrap/javascript/java.js
@@ -2,7 +2,7 @@ let star= "#";
 let btnStart=document.getElementById('btn-start')
 btnStart.addEventListener('click',()=>{
     for(let j=0;j<10;j++){
-        for(let i=1;i<=40;i++){
+        for(let i=1;i<=10;i++){
             if((i%2)==0){
                 let di= i/2;
                 (console.log((star+" ").repeat(di)+star))
@@ -11,7 +11,7 @@ btnStart.addEventListener('click',()=>{
             }
             
         }
-        for(i=40;i>=1;i--){
+        for(i=10;i>=1;i--){
             if((i%2)==0){
                 (console.log((star).repeat(i)))
             }else{
