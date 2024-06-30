84b2cd10019e69925801155cbf9669232ffb158a
sinajr <jamyarisina@gmail.com>
2024-06-30 11:32:58 +0300

    update

--- practice/padst/java.js
+++ practice/padst/java.js
@@ -4,7 +4,6 @@ let userinpt=$.querySelector('#userphone');
 let sec=$.getElementById('sec');
 userinpt.addEventListener('keydown', (e) =>{
     if(e.key==="Enter"){
-        console.log(e)
         sec.insertAdjacentHTML('beforend',`
             <p>
             ${
