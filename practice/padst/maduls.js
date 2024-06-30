f28a95dde8fecadf5ffcc382edc29ce19b3a0c22
sinajr <jamyarisina@gmail.com>
2024-06-30 11:28:15 +0300

    update

--- practice/padst/maduls.js
+++ practice/padst/maduls.js
@@ -2,7 +2,7 @@ var $=document;
 function phonesecret(phoneNum){
 //    let firstNum= phoneNum.slice(0,4)
    let secondNum= phoneNum.slice(9,11)
-   finalnum= secondNum.padStart(11,'*')
+   let finalnum= secondNum.padStart(11,'*')
    return finalnum;
 }
 export{phonesecret}
