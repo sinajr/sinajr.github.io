bf18c9f4be522910c6203bb3157207f513b1ed4a
sinajr <jamyarisina@gmail.com>
2024-06-30 11:32:31 +0300

    update

--- practice/padst/java.js
+++ practice/padst/java.js
@@ -1,9 +1,16 @@
 var $=document;
 import { phonesecret } from "./maduls.js";
 let userinpt=$.querySelector('#userphone');
+let sec=$.getElementById('sec');
 userinpt.addEventListener('keydown', (e) =>{
     if(e.key==="Enter"){
         console.log(e)
-        console.log(phonesecret(userinpt.value))
+        sec.insertAdjacentHTML('beforend',`
+            <p>
+            ${
+            phonesecret(userinpt.value)
+
+            }
+            </p>`)
     }
 })
